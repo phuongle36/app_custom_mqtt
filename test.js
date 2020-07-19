8006ae9a44e4495b14a1509b8722a3972b442cce
Phuong Le <minhphuong9836@gmail.com>
2020-07-19 23:34:24 +0700

    KLTN update

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 var mqtt = require('mqtt');
 var mongo = require('mongodb').MongoClient;
 var mongoUrl = 'mongodb://localhost/';
-var client = mqtt.connect('mqtt://192.168.1.17:1883');
+var client = mqtt.connect('mqtt://127.0.0.1:1883');
 
 client.on('connect', function () {
     client.subscribe('sensorTest');
@@ -12,21 +12,50 @@ client.on('message', function (topic, message) {
     var list = [];
     var fullDate = date.getFullYear() + '/' + (date.getMonth() + 1) + '/' + date.getDate() + ' ' +
     date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds();
+    var check = fullDate.split(' ');
+    var checkDate = check[0].split('/');
+    var checkTime = check[1].split(':');
+    for (var a = 0; a < checkDate.length; a++) {
+        if (checkDate[a].toString().length == 1) {
+            checkDate[a] = '0' + checkDate[a];
+        }
+    }
+
+    for (var a = 0; a < checkTime.length; a++) {
+        if (checkTime[a].toString().length == 1) {
+            checkTime[a] = '0' + checkTime[a];
+        }
+    }
+
+    fullDate = checkDate[0] + '/' + checkDate[1] + '/' + checkDate[2] + ' ' +
+    checkTime[0] + ':' + checkTime[1] + ':' + checkTime[2];
     var context = message.toString();
+    console.log(context);
 
     context = context.split(' ');
+    var kind = context[context.length - 1];
 
-    for (var i = 1; i < context.length; i++) {
-        var split = context[i].split(',');
+    if (kind == 'temperature') {
+        for (var i = 1; i < context.length - 1; i++) {
+            var split = context[i].split(',');
 
-        if (split[0] == '') {
-            split[0] = 0;
+            if (split[0] == '') {
+                split[0] = 0;
+            }
+
+            split[1] = split[1]/100;
+            list.push({
+                id: split[0].toString(),
+                type: kind,
+                value: split[1].toString()
+            });
         }
-
-        split[1] = split[1]/100;
+    } else {
+        context[0] = context[0]/100;
         list.push({
-            id: split[0].toString(),
-            temperature: split[1].toString()
+            id: '2',
+            type: kind,
+            value: context[0].toString()
         });
     }
 
@@ -41,7 +70,7 @@ client.on('message', function (topic, message) {
     mongo.connect(mongoUrl, function (err, db) {
         if (err) throw err;
         var dbo = db.db('test2');
-        dbo.collection('temperatureSensor').insertOne(obj, function (err, res){
+        dbo.collection('temperatureSensor').insert(obj, function (err, res){
             if (err) throw err;
             console.log('1 document inserted');
             db.close();
